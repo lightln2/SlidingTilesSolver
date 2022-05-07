057573c2ef7db5697d0ffdab632c78232f636352
Oleg T <light_ln2@hotmail.com>
2022-05-07 18:41:11 +0300

    4 bits frontier states

--- SlidingTileSolver/FrontierStates.cs
+++ SlidingTileSolver/FrontierStates.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 public class FrontierStates
 {
+    private byte[] States;
 
     private byte[] Bounds;
-    private byte[] States;
 
     public FrontierStates(PuzzleInfo info)
     {
-        States = new byte[info.Total];
+        States = new byte[info.Total / 2];
 
         Bounds = new byte[16];
         for (int i = 0; i < 16; i++)
@@ -34,8 +34,16 @@ public class FrontierStates
         {
             long val = list[i];
             long index = val >> 4;
-            if ((val & PuzzleInfo.STATE_LT) != 0) States[index - 1] |= PuzzleInfo.STATE_RT;
-            if ((val & PuzzleInfo.STATE_RT) != 0) States[index + 1] |= PuzzleInfo.STATE_LT;
+            if (index % 2 == 0)
+            {
+                if ((val & PuzzleInfo.STATE_LT) != 0) States[index / 2 - 1] |= (PuzzleInfo.STATE_RT << 4);
+                if ((val & PuzzleInfo.STATE_RT) != 0) States[index / 2] |= (PuzzleInfo.STATE_LT << 4);
+            }
+            else
+            {
+                if ((val & PuzzleInfo.STATE_LT) != 0) States[index / 2] |= PuzzleInfo.STATE_RT;
+                if ((val & PuzzleInfo.STATE_RT) != 0) States[index / 2 + 1] |= PuzzleInfo.STATE_LT;
+            }
         }
     }
 
@@ -44,7 +52,14 @@ public class FrontierStates
     {
         for (int i = 0; i < count; i++)
         {
-            States[buffer[i]] |= PuzzleInfo.STATE_DN;
+            if (buffer[i] % 2 == 0)
+            {
+                States[buffer[i] / 2] |= PuzzleInfo.STATE_DN;
+            }
+            else
+            {
+                States[buffer[i] / 2] |= (PuzzleInfo.STATE_DN << 4);
+            }
         }
     }
 
@@ -53,7 +68,14 @@ public class FrontierStates
     {
         for (int i = 0; i < count; i++)
         {
-            States[buffer[i]] |= PuzzleInfo.STATE_UP;
+            if (buffer[i] % 2 == 0)
+            {
+                States[buffer[i] / 2] |= PuzzleInfo.STATE_UP;
+            }
+            else
+            {
+                States[buffer[i] / 2] |= (PuzzleInfo.STATE_UP << 4);
+            }
         }
     }
 
@@ -69,19 +91,31 @@ public class FrontierStates
                 ulong val = statesPtrUlong[i];
                 if (val == 0) continue;
                 byte* valPtr = (byte*)&val;
-                long i8 = i * 8;
                 for (int j = 0; j < 8; j++)
                 {
                     byte s = valPtr[j];
                     if (s == 0) continue;
-                    count++;
-                    s |= Bounds[(i8 + j) & 15];
-                    if (s == 15) continue;
-                    list.Add(((i8 + j) << 4) | (byte)(~s & 0xF));
+                    byte s1 = (byte)(s & 15);
+                    byte s2 = (byte)((s >> 4) & 15);
+                    if (s1 != 0) 
+                    {
+                        count++;
+                        s1 |= Bounds[(i * 16 + 2 * j) & 15];
+                        if (s1 == 15) continue;
+                        list.Add(((i * 16 + 2 * j) * 16) | (byte)(~s1 & 0xF));
+                    }
+                    if (s2 != 0)
+                    {
+                        count++;
+                        s2 |= Bounds[(i * 16 + 2 * j + 1) & 15];
+                        if (s2 == 15) continue;
+                        list.Add(((i * 16 + 2 * j + 1) * 16) | (byte)(~s2 & 0xF));
+                    }
                 }
                 statesPtrUlong[i] = 0;
             }
         }
+        //Console.WriteLine(string.Join(" ", list.Take(100)));
         return count;
     }
 }
