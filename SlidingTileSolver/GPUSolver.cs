0925299beb7b16c74730d411981857848298b31c
Oleg T <light_ln2@hotmail.com>
2022-06-16 16:49:51 +0300

    GPU multi-side move

--- SlidingTileSolver/GPUSolver.cs
+++ SlidingTileSolver/GPUSolver.cs
@@ -32,6 +32,7 @@ public unsafe class GpuSolver
     private static Accelerator accelerator;
     private static Action<Index1D, PuzzleParams, ArrayView<long>> life_kernel_up;
     private static Action<Index1D, PuzzleParams, ArrayView<long>> life_kernel_dn;
+    private static Action<Index1D, PuzzleParams, ArrayView<long>, int, int> life_kernel_multimove;
     private static MemoryBuffer1D<long, Stride1D.Dense> dev;
 
     public static long ProcessedValues = 0;
@@ -69,6 +70,58 @@ public unsafe class GpuSolver
         input[index] = VerticalMoveDown(ref p, input[index]);
     }
 
+    static void LifeKernelMultimove(
+        Index1D index,
+        PuzzleParams p,
+        ArrayView<long> input,
+        int count,
+        int row)
+    {
+        byte[] arr1 = new byte[16];
+        byte[] arr2 = new byte[16];
+        byte[] arr3 = new byte[16];
+
+        long value = input[index];
+        FromIndex(ref p, value, arr1);
+        UnpackForMultimove(ref p, arr1);
+        Copy(arr1, arr2);
+
+        Index1D pos = index;
+
+        for (int i = row + 1; i < p.Height; i++)
+        {
+            if (!CanRotateDn(ref p, arr1))
+            {
+                input[pos] = -1;
+            }
+            else
+            {
+                RotateDn(ref p, arr1);
+                Copy(arr1, arr3);
+                Pack(ref p, arr3);
+                input[pos] = GetIndex(ref p, arr3);
+            }
+            pos += count;
+        }
+
+        for (int i = 0; i < row; i++)
+        {
+            if (!CanRotateUp(ref p, arr2))
+            {
+                input[pos] = -1;
+            }
+            else
+            {
+                RotateUp(ref p, arr2);
+                Copy(arr2, arr3);
+                Pack(ref p, arr3);
+                input[pos] = GetIndex(ref p, arr3);
+            }
+            pos += count;
+        }
+
+    }
+
     public static void CompileKernel()
     {
         context = Context.Create(builder =>
@@ -90,6 +143,12 @@ public unsafe class GpuSolver
             Index1D,
             PuzzleParams,
             ArrayView<long>>(LifeKernelDn);
+        life_kernel_multimove = accelerator.LoadAutoGroupedStreamKernel<
+            Index1D,
+            PuzzleParams,
+            ArrayView<long>,
+            int,
+            int>(LifeKernelMultimove);
 
         dev = accelerator.Allocate1D<long>(GPUSIZE);
     }
@@ -169,6 +228,32 @@ public unsafe class GpuSolver
             }
         }
     }
+
+    private static void UnpackForMultimove(ref PuzzleParams p, byte[] arr)
+    {
+        bool invEven = true;
+
+        for (int i = p.Size - 2; i >= 0; i--)
+        {
+            for (int j = i + 1; j < p.Size - 1; j++)
+            {
+                if (arr[j] >= arr[i]) arr[j]++;
+                else invEven = !invEven;
+            }
+        }
+
+        // restore by inversion count
+        byte zeroPos = arr[OFFSET_ZERO];
+        bool rowEven = ((zeroPos / p.Width) & 1) == 0;
+        bool swapLast = (p.WidthIsEven == 1 && invEven == rowEven) || (!(p.WidthIsEven == 1) && invEven);
+        if (swapLast)
+        {
+            byte tmp = arr[p.Size - 2];
+            arr[p.Size - 2] = arr[p.Size - 3];
+            arr[p.Size - 3] = tmp;
+        }
+    }
+
 
     private static void Pack(ref PuzzleParams p, byte[] arr)
     {
@@ -322,6 +407,20 @@ public unsafe class GpuSolver
                 dev.AsContiguous().CopyToCPU(ref indexes[0], count);
                 onStep();
             }
+            GpuExecTime += sw.Elapsed;
+        }
+    }
+
+    public static void CalcGPU_Multimove(int count, long* indexes, int row)
+    {
+        if (count == 0) return;
+        lock (context)
+        {
+            var sw = Stopwatch.StartNew();
+            dev.AsContiguous().CopyFromCPU(ref indexes[0], count);
+            life_kernel_multimove(count, pparams, dev.View, count, row);
+            accelerator.Synchronize();
+            dev.AsContiguous().CopyToCPU(ref indexes[0], count * (pparams.Height - 1));
             GpuExecTime += sw.Elapsed;
         }
     }
